42eb5cbff347c0443524aec3eb281e3ecaf50483
yyoongs <48539539+yyoongs@users.noreply.github.com>
2019-04-08 01:20:56 +0900

    2019.04.08

---
+++ hw1/main.py
@@ -0,0 +1,103 @@
+import numpy as np
+from hw1.mnist import load_mnist
+from hw1 import singlelayer as sn
+from PIL import Image
+
+np.random.seed(1)
+
+
+def img_show(img):  # (784) 혹은 (28, 28)의 mnist 배열을 이미지로 보여주는 작업
+    if img.ndim == 1:
+        img = img.reshape(28, 28)
+        img *= 255.0
+        pil_img = Image.fromarray(np.uint8(img))
+        pil_img.show()
+
+
+def get_data():  # mnist 데이터 받아오기(one_hot_label로)
+    (x_train, y_train), (x_test, y_test) = load_mnist(normalize=True, flatten=True, one_hot_label=True)
+    return x_train, y_train, x_test, y_test
+
+
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def TestSN(input_i, x_train, y_train, x_test, y_test, W,
+           Bias):  # test 이미지를 하나 뽑고 singleNN.py의 trainingAndResult를 돌려서 학습전 결과와 학습 후 결과가 어떻게 다른지 확인하는 것
+    if is_number(input_i):
+        i = int(input_i)    # i에 input_i값을 넣음
+        Test = x_train[i]   # x_train의 i번째 이미지를 test에 넣음
+        label = np.argmax(y_train[i])   #y_train의 i번째 이미지의 최대값의 위치를 label에 넣음
+        # img_show(Test)  #이미지를 보여주는 함수
+        print("이 이미지의 실제 값 : ", label)  # 그림의 숫자와 동일
+
+        SN = sn.singleLayer(W, Bias)    #singlelayer.py에 있는 singlelayer class를 SN으로 설정
+        # y_predict = SN.ScoreFunction(x_train) #y_predict값은 SN의 scorefunction함수에 x_test[i]를 넣어서 나온값
+        # print("y_predict : ", y_predict)
+        # y_soft = SN.Softmax(y_predict)
+        # print("y_soft : ", y_soft.shape)
+        # print("y_train : ", y_train.shape)
+        # y_loss = SN.LossFunction(y_soft, y_train)
+        # print("y_loss : ", y_loss.shape)
+        y_predict,loss = SN.Forward(x_train, y_train)
+        print(y_predict.shape, loss.shape)
+        SN.BackPropagation(x_train, y_predict, y_train)
+        print("이 이미지의 학습 전 이미지의 추론 값 : ", np.argmax(y_predict[i]))  # 추론값의 결과가 그림의 숫자와 같을 수도 다를 수도 있음.
+        SN.Optimization(x_train, y_train, x_test, y_test)   #optimize 실행
+        y_predict = SN.ScoreFunction(x_train[i])    #y_predict는 optimize실시 후의 scorefunction
+        print("학습이 완료되었습니다 \n이미지의 학습 후 추론 값: ", np.argmax(y_predict))  # 트레이닝 후의 추론값 또한 결과값과 다를 수도 있다.(정확도가 87% 정도이기에)
+        return SN
+
+    else:
+        print("잘못 입력하셨습니다. 학습을 하지 않습니다.")
+        return False
+
+
+x_train, y_train, x_test, y_test = get_data()
+
+#
+# print(x_train.shape)
+#
+# print(y_train.shape)
+#
+# print(x_test.shape)
+#
+# print(y_test.shape)
+# 받아온 데이터 형상 출력 test
+
+
+# W값과 Bias 값을 np.random.random 함수를 사용해 알맞게 넣어주십시오.(이 것만 빈칸 나머지는 제공)
+# 3.1
+W = np.random.random((784, 10))  # label의 갯수가 10개이고 이미지당 픽셀수가 784개 이므로 W는 784x10의 벡터값을 가진다
+Bias = np.random.random(10)  # Bias는 각 이미지 label당 하나씩의 값을 가지므로 10개의 값을 가지는 배열
+
+# i = input() # 자신이 원하는 숫자 넣기 가능
+i = 0
+print("train 데이터의 {} 번째의 값 추출".format(i))
+
+Trainend = TestSN(i, x_train, y_train, x_test, y_test, W, Bias)  # 위의 TestNN함수를 호출해 작업을 돌림.
+
+# 밑에 것은 심심하면 자신이 트레이닝한 것이 잘되는지 실험해보세요.
+'''
+if Trainend !=False:
+   TrainNN =Trainend
+   print("몇 번 추론하실 겁니까?")
+   iterator = input()
+   if(is_number(iterator)):
+      for i in range(0, int(iterator)):
+         print("x_train의 s번째 데이터를 뽑아주세요.\n")
+         s = int(input())
+         print("S : {}".format(s))
+         check = x_train[s]
+         img_show(check)
+         Hypothesis = TrainNN.Forward(check)
+         print("이 이미지의 추론 값 : {}".format(np.argmax(Hypothesis)))
+   else:
+      print("iterator로 숫자를 안넣었습니다. 종료합니다.")
+
+'''
---
+++ hw1/singlelayer.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+
+class singleLayer:
+    def __init__(self, W, Bias):  # 제공. 호출 시 작동하는 생성자
+        self.W = W
+        self.B = Bias
+
+    def SetParams(self, W_params, Bias_params):  # 제공. W와 Bias를 바꾸고 싶을 때 쓰는 함수
+        self.W = W_params
+        self.B = Bias_params
+
+    def ScoreFunction(self, X):  # \Score값 계산 -> 직접작성
+        # 3.2
+        # X = x_train[i] or x_train
+        # X의 shape는 (1,784) or (60000,784)
+        # W의 shape는 (784,10)
+        # ScoreMatrix는 (60000,10)이 되어야함
+        ScoreMatrix = np.dot(X, self.W) + self.B
+        return ScoreMatrix
+
+    def Softmax(self, ScoreMatrix):  # 제공.
+        if ScoreMatrix.ndim == 2:
+            temp = ScoreMatrix.T
+            # print("TTTTTTTTTTTT", temp)
+            # print("kkkkkkkkkkkkkkkkk", np.max(temp, axis=0))
+            temp = temp - np.max(temp, axis=0)
+            y_predict = np.exp(temp) / np.sum(np.exp(temp), axis=0)
+            return y_predict.T
+        temp = ScoreMatrix - np.max(ScoreMatrix, axis=0)
+        expX = np.exp(temp)
+        y_predict = expX / np.sum(expX)
+        return y_predict  # 합이1, exp까지 계산됨
+
+    def LossFunction(self, y_predict, Y):  # Loss Function을 구하십시오 -> 직접 작성
+        # 3.3
+        # (Softmax의 결과 값 : y_predict, 정답값 : Y)
+        temp = - np.log(y_predict) * Y
+        loss = temp.sum() / temp.shape[0]
+        return loss
+
+    def Forward(self, X, Y):  # ScoreFunction과 Softmax, LossFunction를 적절히 활용해 y_predict 와 loss를 리턴시키는 함수. -> 직접 작성
+        # 3.4
+        y_score = self.ScoreFunction(X)
+        y_predict = self.Softmax(y_score)
+        print("y_predict : ", y_predict.shape)
+        loss = self.LossFunction(y_predict, Y)
+        return y_predict, loss
+
+    def delta_Loss_Scorefunction(self, y_predict, Y):  # 제공.dL/dScoreFunction
+        delta_Score = y_predict - Y
+        return delta_Score
+    # delta_Score의 shape = (60000,10)
+
+    def delta_Score_weight(self, delta_Score, X):  # 제공. dScoreFunction / dw .
+        delta_W = np.dot(X.T, delta_Score) / X[0].shape
+        # print(np.dot(X.T, delta_Score).shape)
+        # print(X[0].shape)
+        return delta_W
+    # delta_W의 shape = (784,10)
+
+    def delta_Score_bias(self, delta_Score, X):  # 제공. dScoreFunction / db .
+        delta_B = np.sum(delta_Score) / X[0].shape
+        return delta_B
+    # delta_B의 shape = (1)
+
+    # delta 함수를 적절히 써서 delta_w, delta_b 를 return 하십시오.
+    def BackPropagation(self, X, y_predict, Y):
+        # 3.5
+        delta_score = self.delta_Loss_Scorefunction(y_predict, Y)
+        print("delta_score : ",delta_score.shape)
+        delta = self.delta_Score_bias(delta_score, X)
+        print("wwwwwwwwwwwwwww", delta.shape)
+        delta_W = np.dot(self.delta_Score_weight(delta_score, X), delta_score.T)
+        delta_W = delta_W.T
+        print("delta_w : ",delta_W.shape)
+        delta_B = delta_score * self.delta_Score_bias(delta_score, X)
+        # print("delta_b : ", delta_B)
+        return delta_W, delta_B
+
+    # 정확도를 체크하는 Accuracy 제공
+    def Accuracy(self, X, Y):
+        y_score = self.ScoreFunction(X)
+        y_score_argmax = np.argmax(y_score, axis=1)
+        if Y.ndim != 1: Y = np.argmax(Y, axis=1)
+        accuracy = 100 * np.sum(y_score_argmax == Y) / X.shape[0]
+        return accuracy
+
+    # Forward와 BackPropagationAndTraining, Accuracy를 사용하여서 Training을 epoch만큼 시키고, 10번째 트레이닝마다
+    # Training Set의 Accuracy 값과 Test Set의 Accuracy를 print 하십시오
+
+    def Optimization(self, X_train, Y_train, X_test, Y_test, learning_rate=0.01, epoch=100):
+        for i in range(epoch):
+            # 3.6
+            forward_train = self.Forward(X_train, Y_train)
+            y_predict = forward_train[0]
+            loss = forward_train[1]
+
+            backpropo = self.BackPropagation(X_train[i], y_predict, Y_train[i])
+            print(backpropo)
+            # 함수 작성
+            if i % 10 == 0:
+                # 3.6 Accuracy 함수 사용
+                print(i, "번째 트레이닝")
+                print('현재 Loss(Cost)의 값 : ', loss)
+                print("Train Set의 Accuracy의 값 : ", )
+                print("Test Set의 Accuracy의 값 :", )
